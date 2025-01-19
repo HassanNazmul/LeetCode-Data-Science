03730fe547e1d8806125cc52518bffb64106b6f3
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2025-01-19 21:39:40 +0000

    Add SQL script to combine Person and Address tables using LEFT JOIN and NATURAL JOIN

--- MySQL/175_CombineTwoTables.sql
+++ MySQL/175_CombineTwoTables.sql
@@ -1,19 +1,22 @@
 -- 175. Combine Two Tables
+-- First solution: Using explicit LEFT JOIN
 SELECT
-    Person.FirstName,
-    Person.LastName,
-    Address.City,
-    Address.State
+    Person.FirstName,    -- Select first name from Person table
+    Person.LastName,     -- Select last name from Person table
+    Address.City,        -- Select city from Address table
+    Address.State        -- Select state from Address table
 FROM
-    Person
-    LEFT JOIN Address ON Person.PersonId = Address.PersonId;
+    Person              -- Start with Person table
+    LEFT JOIN Address   -- Use LEFT JOIN to include all persons even without address
+    ON Person.PersonId = Address.PersonId;  -- Join condition matching PersonId
 
--- 2nd solution
+-- Second solution: Using NATURAL JOIN
 SELECT
-    FirstName,
-    LastName,
-    City,
-    State -- Select required columns from both tables
+    FirstName,          -- Select first name
+    LastName,          -- Select last name
+    City,              -- Select city
+    State              -- Select state
 FROM
-    Person -- Using NATURAL JOIN to automatically match common columns
-    NATURAL LEFT JOIN Address;
+    Person             -- Start with Person table
+    NATURAL LEFT JOIN  -- Use NATURAL JOIN to automatically match common columns
+    Address;           -- Address table to join with
