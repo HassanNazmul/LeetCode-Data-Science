af99e577ab5b6cdacb2bfc43a49ddda5401753dd
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2025-01-22 22:06:03 +0000

    Refactor combine_two_tables function to include an alternative implementation and add test data for demonstration

--- Pandas/175_CombineTwoTables.py
+++ Pandas/175_CombineTwoTables.py
@@ -1,24 +1,5 @@
 # 175. Combine Two Tables
 
-# Table: Person
-# +-------------+---------+
-# | Column Name | Type    |
-# +-------------+---------+
-# | personId    | int     |
-# | lastName    | varchar |
-# | firstName    | varchar |
-# +-------------+---------+
-
-# Table: Address
-# +-------------+---------+
-# | Column Name | Type    |
-# +-------------+---------+
-# | addressId   | int     |
-# | personId    | int     |
-# | city        | varchar |
-# | state       | varchar |
-# +-------------+---------+
-
 import pandas as pd
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
@@ -34,11 +15,29 @@ def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFr
     
     return result
 
+# # Another way to solve the problem
+# def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+#     # Perform left join on person and address tables based on personId
+#     result = person.merge(address, how='left', on='personId')
 
-    # # Perform left join on person and address tables based on personId
-    # result = person.merge(address, how='left', on='personId')
+#     # Select only firstName, lastName, city, state columns 
+#     result = result[['firstName', 'lastName', 'city', 'state']]
 
-    # # Select only firstName, lastName, city, state columns 
-    # result = result[['firstName', 'lastName', 'city', 'state']]
+#     return result
 
-    # return result
+
+# Test the function
+person = pd.DataFrame({
+    'personId': [1, 2, 3],
+    'lastName': ['Doe', 'Smith', 'Johnson'],
+    'firstName': ['John', 'Jane', 'Alice']
+})
+
+address = pd.DataFrame({
+    'addressId': [1, 2],
+    'personId': [1, 3],
+    'city': ['New York', 'Chicago'],
+    'state': ['NY', 'IL']
+})
+
+print(combine_two_tables(person, address))
